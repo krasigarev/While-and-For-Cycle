3d4cfe24488f7ef2c8b6248b0f5752a26d5457f4
krasigarev <kgarev@gmail.com>
2024-07-23 19:30:02 +0300

    first commit

--- relocation.py
+++ relocation.py
@@ -3,7 +3,16 @@ length = int(input())
 height = int(input())
 
 all_space = width * length * height
+boxes = None
 
 while all_space > 0:
-    boxes = int(input())
-    all_space -= boxes
+    boxes = input()
+
+    if boxes == "Done":
+        print(f"{all_space} Cubic meters left.")
+        break
+
+    all_space -= int(boxes)
+
+if not boxes == "Done":
+    print(f"No more free space! You need {abs(all_space)} Cubic meters more.")
