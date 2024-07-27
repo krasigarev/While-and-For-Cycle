13a74d6ec49f83ac14a557f759f9e16d5104c65a
krasigarev <kgarev@gmail.com>
2024-07-27 14:45:08 +0300

    nasted cicle

--- number_sum.py
+++ number_sum.py
@@ -1,19 +1,22 @@
-n = int(input())
+# n = int(input())
+#
+# left_sum = 0
+# right_sum = 0
+#
+# for num in range(n):
+#     current_num = int(input())
+#     left_sum += current_num
+#
+# for num in range(n):
+#     current_num = int(input())
+#     right_sum += current_num
+#
+# if left_sum == right_sum:
+#     print(f"Yes, sum = {left_sum}")
+# else:
+#     diff = abs(left_sum - right_sum)
+#     print(f"No difference = {diff}")
+#
 
-left_sum = 0
-right_sum = 0
-
-for num in range(n):
-    current_num = int(input())
-    left_sum += current_num
-
-for num in range(n):
-    current_num = int(input())
-    right_sum += current_num
-
-if left_sum == right_sum:
-    print(f"Yes, sum = {left_sum}")
-else:
-    diff = abs(left_sum - right_sum)
-    print(f"No difference = {diff}")
-
+for i in range(0, 2):
+    print(i)
