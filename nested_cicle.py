bb0d0b3adf00669eba85fdcb6abc2aa29151ac81
krasigarev <kgarev@gmail.com>
2024-07-27 16:31:15 +0300

    nasted cicle

--- nested_cicle.py
+++ nested_cicle.py
@@ -2,3 +2,12 @@ for hour in range(0, 24):
     for minute in range(0, 60):
         for seconds in range(0, 60):
             print(f"{hour}:{minute}:{seconds}")
+
+n = int(input())
+
+num = 0
+
+for i in range(1, n):
+    if True:
+        num += 3
+        print(num)
