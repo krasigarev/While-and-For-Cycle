09e7b7719b1b671cca3ba83dafd585f0ef9c053a
krasigarev <kgarev@gmail.com>
2024-07-27 20:28:43 +0300

    nasted cicle

---
+++ magic_number.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+for x1 in range(0, 10):
+    for x2 in range(0, 10):
+        for x3 in range(0, 10):
+            for x4 in range(0, 10):
+                for x5 in range(0, 10):
+                    for x6 in range(0, 10):
+                        if x1 * x2 * x3 * x4 * x5 * x6 == n:
+                            print(f"{x1}{x2}{x3}{x4}{x5}{x6} ", end="")
