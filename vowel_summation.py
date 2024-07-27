2ca094599a630c1bedd51f22033e1455ae87ecec
krasigarev <kgarev@gmail.com>
2024-07-27 11:32:39 +0300

    latter

--- vowel_summation.py
+++ vowel_summation.py
@@ -16,3 +16,18 @@ for latter in word:
 
 print(result)
 
+
+for index in range(0, len(word)):
+    if word[index] == "a":
+         result += 1
+    elif word[index] == "e":
+         result += 2
+    elif word[index] == "i":
+         result += 3
+    elif word[index] == "o":
+         result += 4
+    elif word[index] == "u":
+         result += 5
+
+
+print(result)
